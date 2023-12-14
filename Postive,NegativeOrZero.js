54124845c0ff87390e300b95339b958b5d20e2b9
Govind Patidar <gppatel841@gmail.com>
2023-12-14 19:36:28 +0530

    Number Positive , Negative or Zero

---
+++ Postive,NegativeOrZero.js
@@ -0,0 +1,13 @@
+// Program to Check if a number is Positive, Negative, or Zero
+
+var input= -10;
+
+if(input>0){
+   console.log("Number is Positive")
+} else if (input<0) {
+   console.log("Number is Negative")
+   
+} else {
+   console.log("Number is Zero")
+   
+}
