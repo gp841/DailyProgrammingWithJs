ca248446e3c240357c309da5755dc13476683a76
Govind Patidar <gppatel841@gmail.com>
2023-12-14 19:33:24 +0530

    Largest Number in Three Number

---
+++ largestInThree.js
@@ -0,0 +1,17 @@
+// Find the largest number among three numbers
+
+let a=55;
+let b= 15;
+let c= 25;
+
+if(a>b && a>c){
+   console.log(a)
+}
+else if(b>a && b>c){
+   console.log(b)
+}
+else if (c>a && c>b){
+   console.log(c)
+}
+
+
